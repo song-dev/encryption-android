9d8a2ddc77a80354258a1d8fbca8badfad62d523
song-dev <fengyas_ong@163.com>
2020-01-16 11:45:13 +0800

    [feature/0.1.0][update][ sm4 对称性加密 java 单元测试 ]

---
+++ app/src/main/java/com/song/encryption/GMBaseUtil.java
@@ -0,0 +1,11 @@
+package com.song.encryption;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Security;
+
+public class GMBaseUtil {
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+}
